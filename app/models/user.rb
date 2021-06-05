058d2c118afef0f31e7ce8fe65c47283bb97cffb
nomikazu <>
2021-06-05 22:58:57 +0900

    いいね実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  def has_liked?(post)
+    likes.exists?(post_id: post.id)
+  end
+
   def prepare_profile
     profile || build_profile
   end
