429f6fcff2fc06076c30becd155051de534d82b9
nomikazu <>
2021-06-05 19:50:08 +0900

    post-new作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,44 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @post = current_user.posts.build
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save!
+      redirect_to posts_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, images: [])
   end
 end
