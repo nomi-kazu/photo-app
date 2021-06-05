0e4fbc4838ee1f80f6b0f0b71f7f5c834e2c9d1d
nomikazu <>
2021-06-05 21:47:59 +0900

    likeモデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
   has_one_attached :avatar, dependent: :destroy
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def prepare_profile
     profile || build_profile
